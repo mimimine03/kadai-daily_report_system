bceb2cc2b49fadebc38e75f2f2ffa729b931f0a3
daisuke.mine <tensaidaisuke03@gmail.com>
2022-07-10 21:09:27 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -64,6 +64,7 @@ public class ReportAction extends ActionBase {
         //一覧画面を表示
         forward(ForwardConst.FW_REP_INDEX);
     }
+
     /**
      * 新規登録画面を表示する
      * @throws ServletException
@@ -80,7 +81,8 @@ public class ReportAction extends ActionBase {
 
         //新規登録画面を表示
         forward(ForwardConst.FW_REP_NEW);
-        }
+    }
+
     /**
      * 新規登録を行う
      * @throws ServletException
@@ -93,9 +95,9 @@ public class ReportAction extends ActionBase {
             //日報の日付が入力されていなければ、今日の日付を設定
             LocalDate day = null;
             if (getRequestParam(AttributeConst.REP_DATE) == null
-                || getRequestParam(AttributeConst.REP_DATE).equals("")){
+                    || getRequestParam(AttributeConst.REP_DATE).equals("")) {
                 day = LocalDate.now();
-            }else {
+            } else {
                 day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
             }
 
@@ -105,7 +107,7 @@ public class ReportAction extends ActionBase {
             //パラメータの値をもとに日報情報のインスタンスを作成する
             ReportView rv = new ReportView(
                     null,
-                    ev,//ログインしている従業員を、日報作成者として登録する
+                    ev, //ログインしている従業員を、日報作成者として登録する
                     day,
                     getRequestParam(AttributeConst.REP_TITLE),
                     getRequestParam(AttributeConst.REP_CONTENT),
@@ -121,18 +123,41 @@ public class ReportAction extends ActionBase {
                 putRequestScope(AttributeConst.REPORT, rv);//入力された日報情報
                 putRequestScope(AttributeConst.ERR, errors);//エラーのリスト
 
-              //新規登録画面を再表示
+                //新規登録画面を再表示
                 forward(ForwardConst.FW_REP_NEW);
 
             } else {
                 //登録中にエラーがなかった場合
 
                 //セッションに登録完了のフラッシュメッセージを設定
-                putSessionScope(AttributeConst.FLUSH,MessageConst.I_REGISTERED.getMessage());
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
 
                 //一覧画面にリダイレクト
-                redirect(ForwardConst.ACT_REP,ForwardConst.CMD_INDEX);
+                redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
             }
+        }
+    }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に日報データを取得する
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            //該当の日報データが存在しない場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.REPORT, rv); //取得した日報データ
+
+            //詳細画面を表示
+            forward(ForwardConst.FW_REP_SHOW);
         }
     }
 }
